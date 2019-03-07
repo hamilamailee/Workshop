64090d2284970ed432ad774396cea57874b79919
hamilamailee <hamilamailee@yahoo.com>
2019-03-07 09:57:06 +0330

    esmeto begoo ^_^

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
         System.out.println("this is our repository ^_^");
         System.out.println("yeg");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("in kie k alan dare taghir mide ???");
+        String name = scanner.next();
+        System.out.println("salam " + name);
     }
 }
