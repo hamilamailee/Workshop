6700681a803b0dfd26cf71bb45a3688964f274c6
hamilamailee <hamilamailee@yahoo.com>
2019-03-07 09:29:49 +0330

    a little change ^_^

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args){
-        System.out.println("this is pur repository ^_^");
+        System.out.println("this is our repository ^_^");
     }
 }
